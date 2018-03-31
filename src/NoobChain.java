ffa83cbee00bac5479a1641778b085af015b1408
paulo.almeida.junior <paulo.almeida.junior@accenture.com>
2018-03-30 23:46:00 -0300

    gitignore

--- src/NoobChain.java
+++ src/NoobChain.java
@@ -12,7 +12,7 @@ public class NoobChain {
 	final static Logger LOG = Logger.getLogger(NoobChain.class);
 
 	public static ArrayList<Block> blockchain = new ArrayList<Block>();
-	public static int difficulty = 9;
+	public static int difficulty = 5;
 
 	public static void main(String[] args) {
 		// add our blocks to the blockchain ArrayList:
